98a09f36cf35d8d91b9606683a8d60bc012f5f5b
Ravikiran-Yenduva <75813398+Ravikiran-Yenduva@users.noreply.github.com>
2021-11-29 11:43:44 +0000

    Update provider.tf

--- TerraformCloud/provider.tf
+++ TerraformCloud/provider.tf
@@ -1,4 +1,8 @@
 //Provider
 provider "azurerm" {
+  client_id = var.client_id
+  subscription_id = var.subscription_id
+  tenant_id = var.tenant_id
+  client_secret = var.client_secret
   features {}
 }
