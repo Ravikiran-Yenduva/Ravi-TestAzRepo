8a04a7585edbe8d40c0fa5595a5d69cb6b2191ae
Ravikiran-Yenduva <75813398+Ravikiran-Yenduva@users.noreply.github.com>
2021-11-29 12:15:10 +0000

    Create variables.tf

---
+++ TerraformCloud/variables.tf
@@ -0,0 +1,16 @@
+variable "client_id" {
+  type        = string
+  description = "client_id"
+}
+variable "subscription_id" {
+  type        = string
+  description = "subscription_id"
+}
+variable "tenant_id" {
+  type        = string
+  description = "tenant_id"
+}
+variable "client_secret" {
+  type        = string
+  description = "client_secret"
+}
